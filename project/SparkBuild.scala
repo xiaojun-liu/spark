c661e0823db8d403b18ee6dc3e460b06fa39bde0
XJ Liu <xj@cloudphysics.com>
2014-05-27 19:42:41 -0700

    Customize Hadoop/HBase versions.

--- project/SparkBuild.scala
+++ project/SparkBuild.scala
@@ -38,7 +38,7 @@ object SparkBuild extends Build {
   // Hadoop version to build against. For example, "1.0.4" for Apache releases, or
   // "2.0.0-mr1-cdh4.2.0" for Cloudera Hadoop. Note that these variables can be set
   // through the environment variables SPARK_HADOOP_VERSION and SPARK_YARN.
-  val DEFAULT_HADOOP_VERSION = "1.0.4"
+  val DEFAULT_HADOOP_VERSION = "1.0.3"
 
   // Whether the Hadoop version to build against is 2.2.x, or a variant of it. This can be set
   // through the SPARK_IS_NEW_HADOOP environment variable.
@@ -49,7 +49,7 @@ object SparkBuild extends Build {
   val DEFAULT_HIVE = false
 
   // HBase version; set as appropriate.
-  val HBASE_VERSION = "0.94.6"
+  val HBASE_VERSION = "0.92.0"
 
   // Target JVM version
   val SCALAC_JVM_VERSION = "jvm-1.6"
